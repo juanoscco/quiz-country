42ce625c34c1cc82625305fa8255fe8adba7283e
Juan Oscco <juan.osccomori@gmail.com>
2023-10-07 00:30:57 -0500

    usign react hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,268 @@
-import Image from 'next/image'
+"use client";
+import React, { useEffect, useState } from "react";
+import clsx from "clsx";
+import { Montserrat } from "next/font/google";
+import Image from "next/image";
+import quizAdventure from "@/assets/icons/undraw_adventure_4hum-1.svg";
+import winnerImage from "@/assets/icons/undraw_winners_ao2o-2.svg";
+
+interface QuizData {
+  question: string;
+  choices: string[];
+  correctAnswer: string;
+  image?: string;
+}
+
+const montserrat = Montserrat({
+  weight: ["700", "500"],
+  subsets: ["cyrillic"],
+});
 
 export default function Home() {
+  const [quizData, setQuizData] = useState<QuizData>({
+    question: "",
+    choices: [],
+    correctAnswer: "",
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+  const [resultCount, setResultCount] = useState<number>(0);
+  const [showResult, setShowResult] = useState<boolean>(false);
+  const [isFinished, setIsFinished] = useState<boolean>(false);
+
+  useEffect(() => {
+    generateRandomQuiz();
+  }, []);
+
+  const generateRandomQuiz = () => {
+    setIsLoading(true);
+
+    const randomQuestionType = Math.floor(Math.random() * 2);
+
+    fetch("https://restcountries.com/v3.1/all?fields=name,flags,capital")
+      .then((response) => response.json())
+      .then((data) => {
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const randomCountry = data[randomIndex];
+
+        let formattedQuizData: QuizData = {
+          question: "",
+          choices: [],
+          correctAnswer: "",
+        };
+        if (randomQuestionType === 0) {
+          formattedQuizData = {
+            question: `Which country does this flag belong to?`,
+            choices: [
+              randomCountry.name.common,
+              getRandomCountry(data),
+              getRandomCountry(data),
+              getRandomCountry(data),
+            ],
+            correctAnswer: randomCountry.name.common,
+            image: randomCountry.flags.png,
+          };
+        } else {
+          formattedQuizData = {
+            question: `${randomCountry.name.common} is the capital of`,
+            choices: [
+              randomCountry.capital,
+              getRandomCapital(data),
+              getRandomCapital(data),
+              getRandomCapital(data),
+            ],
+            correctAnswer: randomCountry.capital,
+          };
+        }
+
+        formattedQuizData.choices = shuffleArray(formattedQuizData.choices);
+
+        setQuizData(formattedQuizData);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("API request failed:", error);
+        setIsLoading(false);
+      });
+  };
+
+  const shuffleArray = (array: string[]) => {
+    const shuffledArray = array.slice();
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
+
+  const getRandomCountry = (allCountries: any[]) => {
+    const randomIndex = Math.floor(Math.random() * allCountries.length);
+    return allCountries[randomIndex].name.common;
+  };
+
+  const getRandomCapital = (allCountries: any[]) => {
+    const randomIndex = Math.floor(Math.random() * allCountries.length);
+    const capitalArray = allCountries[randomIndex].capital;
+
+    if (Array.isArray(capitalArray) && capitalArray.length > 0) {
+      return capitalArray[0];
+    } else {
+      return "There is no capital";
+    }
+  };
+
+  const handleNextQuestion = () => {
+    generateRandomQuiz();
+    setIsCorrect(false);
+    setShowResult(false);
+  };
+
+  const handleAnswerClick = (answer: string) => {
+    setIsCorrect(true);
+    setSelectedAnswer(answer);
+    if (answer === quizData.correctAnswer) {
+      setResultCount(resultCount + 1);
+    } else {
+      setIsFinished(true);
+    }
+  };
+
+  const handleFinishQuiz = () => {
+    setShowResult(true);
+  };
+
+  const handleTryAgain = () => {
+    handleNextQuestion();
+    setResultCount(0);
+    setIsFinished(false);
+  };
+
+  useEffect(() => {
+    console.log("Correct Answer:", quizData.correctAnswer);
+  }, [quizData]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="h-screen mx-auto max-w-[30rem] flex flex-col justify-center gap-5 px-2">
+      <h1 className="uppercase text-2xl font-bold sm:text-4xl text-white">
+        Country Quiz
+      </h1>
+
+      <section
+        className={`animete-custom bg-white text-blue-500 px-8  rounded-3xl`}
+      >
+        {isLoading ? (
+          <div className="animate-pulse h-96 flex flex-col items-center justify-center">
+          <div className="h-4 bg-gray-300 rounded w-3/4 mx-auto mb-4"></div>
+          <div className="h-4 bg-gray-300 rounded w-3/4 mx-auto mb-4"></div>
+          <div className="h-4 bg-gray-300 rounded w-3/4 mx-auto mb-4"></div>
+          <div className="h-4 bg-gray-300 rounded w-3/4 mx-auto mb-4"></div>
+          {/* Agrega más elementos de esqueleto según sea necesario */}
         </div>
-      </div>
+        ) : !showResult ? (
+          <div className={`loaded-content pt-12 relative duration-500 `}>
+            <Image
+              className="absolute right-0 top-0 translate-x-8 -translate-y-[4.583rem]"
+              src={quizAdventure}
+              alt="quiz-adventure"
+              priority={true}
+              width={162}
+              height={116}
+            />
+            {quizData.image && (
+              <img className="w-24 mb-6" src={quizData.image} alt="Flag" />
+            )}
+            <p
+              className="text-xl sm:text-2xl font-bold mb-8"
+              style={{ color: "#2F527B" }}
+            >
+              {quizData.question}
+            </p>
+            <ul>
+              {quizData.choices.map((choice, index) => (
+                <li
+                  className={clsx(
+                    "cursor-pointer rounded-xl flex gap-9 text-lg px-5 py-2 border-2 border-blue-600 mb-6 ",
+                    choice === selectedAnswer
+                      ? selectedAnswer === quizData.correctAnswer
+                        ? "bg-green-500 text-white border-green-400"
+                        : "bg-red-400 text-white border-red-400"
+                      : "",
+                    !isCorrect &&
+                      "hover:bg-orange-400 hover:text-white hover:border-orange-400 active:bg-orange-300 duration-200",
+                    isCorrect &&
+                      choice == quizData.correctAnswer &&
+                      "border-green-400 bg-green-400 text-white"
+                  )}
+                  key={index}
+                  onClick={() => handleAnswerClick(choice)}
+                >
+                  <span className="text-2xl ">
+                    {String.fromCharCode(65 + index)}
+                  </span>
+                  <p className="">{choice}</p>
+                </li>
+              ))}
+            </ul>
+            <div className="flex justify-end pb-8">
+              <button
+                className={clsx(
+                  "text-white bg-yellow-500 px-9 py-3 rounded-xl",
+                  !isCorrect && "hidden"
+                )}
+                onClick={isFinished ? handleFinishQuiz : handleNextQuestion}
+              >
+                <p className="text-lg font-bold text-white">
+                  {isFinished ? "Finish" : "Next"}
+                </p>
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="text-center">
+            <Image
+              src={winnerImage}
+              width={238}
+              height={136}
+              className="mx-auto pt-10"
+              alt="winner-svg"
+              priority={true}
+            />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <div className="my-16">
+              <h1 className="text-5xl text-blue-900 font-bold mb-4">Results</h1>
+              <p>
+                You got{" "}
+                <span className="text-4xl text-green-500 font-bold">
+                  {resultCount}
+                </span>{" "}
+                correct answers
+              </p>
+            </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+            <button
+              onClick={handleTryAgain}
+              className="mb-8 px-14 py-4 text-blue-900 hover:bg-blue-600 hover:text-white rounded-xl duration-200 border-2 border-blue-900 hover:border-blue-600 text-lg font-semibold"
+            >
+              Try again
+            </button>
+          </div>
+        )}
+      </section>
+      <footer className={`${montserrat.className} pt-20 text-center text-white`}>
+        created by{" "}
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="http://Github.com/jcom-dev"
+          className="font-bold"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+          Juan Oscco Mori
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        - devChallenges.io
+      </footer>
     </main>
-  )
+  );
 }
